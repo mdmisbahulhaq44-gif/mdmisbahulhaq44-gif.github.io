787dd5eb8b347e18d3a52ef7d65a65caf27e0871
mdmisbahulhaq44-gif <mdmisbahulhaq44@gmail.com>
2026-09-09 14:30:51 +0600

    Harden service worker caching, add temporary debug overlay

--- service-worker.js
+++ service-worker.js
@@ -22,7 +22,7 @@ self.addEventListener("fetch", event => {
 if(event.request.method !== "GET") return
 
 event.respondWith(
-fetch(event.request)
+fetch(event.request, { cache: "no-store" })
 .then(response => {
 const copy = response.clone()
 caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy))
